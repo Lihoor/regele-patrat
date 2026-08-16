4fc9d3f4db9535478697354b83c0ba2f782c4bc4
Lihoor <koujuuk@gmail.com>
2026-08-16 18:33:26 +0300

    Mers mai lent, fara sunet la saritura, pasi mai rari

--- js/entities/king.js
+++ js/entities/king.js
@@ -8,7 +8,7 @@ class King {
     this.y = level.groundY - this.h;
     this.vy = 0;
     this.onGround = true;
-    this.speed = 380;
+    this.speed = 300;
     this.jumpV = -800;
     this.gravity = 1750;
     this.facing = 1;
@@ -52,7 +52,7 @@ class King {
 
     if (this.moving) {
       this.stepAcc += speed * dt;
-      const stride = this.sprinting ? 95 : 55;
+      const stride = this.sprinting ? 170 : 135;
       if (this.stepAcc >= stride) {
         this.stepAcc -= stride;
         if (this.sound) this.sound.footstep(this.sprinting);
@@ -70,7 +70,6 @@ class King {
       const wasAir = !this.onGround;
       this.y = level.groundY - this.h;
       if (wasAir && fx) fx.burst(this.x + this.w / 2, level.groundY, 7);
-      if (wasAir && this.sound) this.sound.play("land", 1, 0.4);
       this.vy = 0;
       this.onGround = true;
       this.squashT = 0.14;
@@ -87,7 +86,6 @@ class King {
     if (this.onGround) {
       this.vy = this.jumpV;
       this.onGround = false;
-      if (this.sound) this.sound.play("jump", 1, 0.3);
     }
   }
 
