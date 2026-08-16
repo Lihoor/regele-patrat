60eb3d957ed55f722d1c31644a2032d643f9541b
Lihoor <koujuuk@gmail.com>
2026-08-16 20:47:00 +0300

    Mutat in core/

---
+++ js/core/input.js
@@ -0,0 +1,55 @@
+class Input {
+  constructor() {
+    this.left = false;
+    this.right = false;
+    this.sprint = false;
+    this._jumpQueued = false;
+    this._interactQueued = false;
+    this._equipSlot = -1;
+
+    window.addEventListener("keydown", (e) => this.onKey(e, true));
+    window.addEventListener("keyup", (e) => this.onKey(e, false));
+  }
+
+  onKey(e, down) {
+    if (e.code === "KeyA" || e.code === "ArrowLeft") {
+      e.preventDefault();
+      this.left = down;
+    }
+    if (e.code === "KeyD" || e.code === "ArrowRight") {
+      e.preventDefault();
+      this.right = down;
+    }
+    if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+      this.sprint = down;
+    }
+    if (e.code === "Space" || e.code === "ArrowUp") {
+      e.preventDefault();
+      if (down) this._jumpQueued = true;
+    }
+    if (e.code === "KeyE") {
+      if (down) this._interactQueued = true;
+    }
+    if (down && e.code === "Digit1") this._equipSlot = 0;
+    if (down && e.code === "Digit2") this._equipSlot = 1;
+    if (down && e.code === "Digit3") this._equipSlot = 2;
+  }
+
+  consumeJump() {
+    const j = this._jumpQueued;
+    this._jumpQueued = false;
+    return j;
+  }
+
+  consumeInteract() {
+    const v = this._interactQueued;
+    this._interactQueued = false;
+    return v;
+  }
+
+  consumeEquip() {
+    const v = this._equipSlot;
+    this._equipSlot = -1;
+    return v;
+  }
+}
