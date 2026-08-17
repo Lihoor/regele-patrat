d3e6d4717a700c07ceef6f40852eb0b8e97063de
Lihoor <koujuuk@gmail.com>
2026-08-17 09:24:31 +0300

    Add bow sound effect

--- js/core/sound.js
+++ js/core/sound.js
@@ -12,11 +12,11 @@ class Sound {
     if (!AC) return;
     this.ctx = new AC();
     if (!window.fetch) return;
-    const names = ["step", "step_fast", "land", "jump", "creak", "sneeze"];
+    const names = ["step", "step_fast", "land", "jump", "creak", "sneeze", "bow"];
     for (let i = 0; i < names.length; i++) {
       const name = names[i];
       try {
-        fetch("sounds/" + name + ".wav?v=4")
+        fetch("sounds/" + name + ".wav?v=5")
           .then((r) => r.arrayBuffer())
           .then((b) => this.ctx.decodeAudioData(b, (buf) => { this.buffers[name] = buf; this.loaded = true; }))
           .catch(() => {});
